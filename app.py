4547434ce4ccf0847639b7517328a39762a3a703
LucasR <lucasreynaud4@gmail.com>
2023-12-18 08:23:09 +0100

    Connexion + event à venir

--- app.py
+++ app.py
@@ -1,10 +1,84 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, session, request, render_template
+from google_auth_oauthlib.flow import Flow
+import os
+from googleapiclient.discovery import build
+import datetime
+from google.oauth2 import credentials as google_credentials
 
 app = Flask(__name__)
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+
+# Générer une clé secrète forte
+app.secret_key = os.urandom(24)
 
 @app.route("/")
 def about():
     return render_template("index.html")
+
+@app.route('/login')
+def login():
+    flow = Flow.from_client_secrets_file(
+        'client_secret.json',
+        scopes=['https://www.googleapis.com/auth/calendar'],
+        redirect_uri=url_for('oauth2callback', _external=True))
+
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        include_granted_scopes='true')
+
+    session['state'] = state
+
+    return redirect(authorization_url)
+
+@app.route('/oauth2callback')
+def oauth2callback():
+    state = session.get('state')
+
+    if not state:
+        return "État de session manquant", 400
+
+    flow = Flow.from_client_secrets_file(
+        'client_secret.json',
+        scopes=['https://www.googleapis.com/auth/calendar'],
+        state=state,
+        redirect_uri=url_for('oauth2callback', _external=True))
+
+    flow.fetch_token(authorization_response=request.url)
+
+    credentials = flow.credentials
+    session['credentials'] = credentials_to_dict(credentials)
+
+    return redirect(url_for('show_events'))
+
+@app.route('/show_events')
+def show_events():
+    # Reconstituer l'objet Credentials à partir des données de la session
+    credentials = google_credentials.Credentials(
+        **session['credentials'])
+
+    service = create_calendar_service(credentials)
+    events = list_upcoming_events(service)
+
+    return render_template('events.html', events=events)
+
+def credentials_to_dict(credentials):
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes}
+
+def create_calendar_service(credentials):
+    return build('calendar', 'v3', credentials=credentials)
+
+def list_upcoming_events(service, max_results=10):
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                          maxResults=max_results, singleEvents=True,
+                                          orderBy='startTime').execute()
+    return events_result.get('items', [])
 
 if __name__ == "__main__":
     app.run(debug=True)
